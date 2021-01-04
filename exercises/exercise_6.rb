7c2403de5b9a1c8930c4976baa3543aa59d6ed4b
Charles P <charles.prystupa@gmail.com>
2021-01-04 23:20:38 +0000

    Validation

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -13,16 +13,16 @@ Store.has_many :employees
 Employee.belongs_to :store
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store1.employees.create(first_name: "Joe", last_name: "Frank", hourly_rate: 20)
-@store1.employees.create(first_name: "Terry", last_name: "Jack", hourly_rate: 20)
-@store1.employees.create(first_name: "Fitz", last_name: "Henry", hourly_rate: 25)
-@store1.employees.create(first_name: "Henry", last_name: "Fitz", hourly_rate: 25)
-@store1.employees.create(first_name: "Smith", last_name: "Henry", hourly_rate: 25)
+@store1.employees.create(first_name: "Joe", last_name: "Frank", hourly_rate: 40)
+@store1.employees.create(first_name: "Terry", last_name: "Jack", hourly_rate: 40)
+@store1.employees.create(first_name: "Fitz", last_name: "Henry", hourly_rate: 45)
+@store1.employees.create(first_name: "Henry", last_name: "Fitz", hourly_rate: 45)
+@store1.employees.create(first_name: "Smith", last_name: "Henry", hourly_rate: 45)
 
 @store2.employees.create(first_name: "Joe", last_name: "Rani", hourly_rate: 65)
-@store2.employees.create(first_name: "Harry", last_name: "Vir", hourly_rate: 30)
-@store2.employees.create(first_name: "Sally", last_name: "ASD", hourly_rate: 25)
-@store2.employees.create(first_name: "Terry", last_name: "Ios", hourly_rate: 22)
-@store2.employees.create(first_name: "Terry", last_name: "Smith", hourly_rate: 22)
-@store2.employees.create(first_name: "John", last_name: "REs", hourly_rate: 20)
+@store2.employees.create(first_name: "Harry", last_name: "Vir", hourly_rate: 40)
+@store2.employees.create(first_name: "Sally", last_name: "ASD", hourly_rate: 45)
+@store2.employees.create(first_name: "Terry", last_name: "Ios", hourly_rate: 42)
+@store2.employees.create(first_name: "Terry", last_name: "Smith", hourly_rate: 42)
+@store2.employees.create(first_name: "John", last_name: "REs", hourly_rate: 40)
 
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,33 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# Employees must always have a first name present
+# Employees must always have a last name present
+# Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+Employee.validates :first_name, :last_name, :store, presence: true
+
+# Employees have a hourly_rate that is a number (integer) between 40 and 200
+Employee.validates :hourly_rate, numericality: {greater_than: 39,  less_than: 201}
+# test = @store1.employees.create(first_name: "test", last_name:"Test2", hourly_rate: 40).valid?
+# puts test
+
+
+# Stores must always have a name that is a minimum of 3 characters
+Store.validates :name, length: {minimum: 3 }
+# Stores have an annual_revenue that is a number (integer) that must be 0 or more
+Store.validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+# BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
+
+
+# User Inputs Store name
+puts 'Input a store name:'
+@test_store = gets.chomp
+
+# Make above Store with name only
+store = Store.create(name: @test_store)
+
+#Display Error(s)
+store.errors.messages.each do |key, msg|
+  puts "Error: #{key} > #{msg}"
+end
