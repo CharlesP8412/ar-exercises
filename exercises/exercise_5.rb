fb7a5af03d80c0312f166751407305f71b1fdb32
Charles P <charles.prystupa@gmail.com>
2021-01-04 21:48:23 +0000

    Avg, Sum and Cond. Query

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum('annual_revenue')
+@avg_revenue = Store.average('annual_revenue')
+@stores_over_1mil = Store.where(['annual_revenue > ?', 1_000_000]).size
+puts "Total Revenue: $#{@total_revenue}"
+puts "Average Store Revenue: $#{@avg_revenue}"
+puts "#{@stores_over_1mil} Stores with Revenue Over $1 Million"
