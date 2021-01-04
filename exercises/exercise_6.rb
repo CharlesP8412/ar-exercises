46320a8e1013c44f546c0f55dc915dc57b5c6502
Charles P <charles.prystupa@gmail.com>
2021-01-04 21:56:05 +0000

    Link Tables and create employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,20 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+Store.has_many :employees
+Employee.belongs_to :store
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Joe", last_name: "Frank", hourly_rate: 20)
+@store1.employees.create(first_name: "Terry", last_name: "Jack", hourly_rate: 20)
+@store1.employees.create(first_name: "Fitz", last_name: "Henry", hourly_rate: 25)
+@store1.employees.create(first_name: "Henry", last_name: "Fitz", hourly_rate: 25)
+@store1.employees.create(first_name: "Smith", last_name: "Henry", hourly_rate: 25)
+
+@store2.employees.create(first_name: "Joe", last_name: "Rani", hourly_rate: 65)
+@store2.employees.create(first_name: "Harry", last_name: "Vir", hourly_rate: 30)
+@store2.employees.create(first_name: "Sally", last_name: "ASD", hourly_rate: 25)
+@store2.employees.create(first_name: "Terry", last_name: "Ios", hourly_rate: 22)
+@store2.employees.create(first_name: "Terry", last_name: "Smith", hourly_rate: 22)
+@store2.employees.create(first_name: "John", last_name: "REs", hourly_rate: 20)
+
