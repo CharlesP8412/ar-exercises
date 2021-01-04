6707f50d5a819512d6423c52c0bdea63be80f8a1
Charles P <charles.prystupa@gmail.com>
2021-01-04 21:34:54 +0000

    Where Query

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,20 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store = Store.create(name: 'Surrey', annual_revenue: 224_000, mens_apparel: false, womens_apparel: true)
+store = Store.create(name: 'Whistler', annual_revenue: 1_900_000, mens_apparel: true, womens_apparel: false)
+store = Store.create(name: 'Yaletown', annual_revenue: 430_000, mens_apparel: true, womens_apparel: true)
+
+# fetch Only Mens Stores
+@mens_stores = Store.where(["mens_apparel = ? and womens_apparel = ?", true, false])
+@mens_stores.each do |mens_store|
+  puts " #{mens_store.name}, Annual Revenue: #{mens_store.annual_revenue}"
+end
+
+# fetch Only Womens Stores w. < 1mil revenue
+@womens_stores = Store.where(["mens_apparel = ? and womens_apparel = ?", false, true])
+@womens_stores.each do |womens_store|
+  if womens_store.annual_revenue < 1_000_000
+    puts " #{womens_store.name}, Annual Revenue: #{womens_store.annual_revenue}"
+  end
+end
